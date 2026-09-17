799fbea7ef4511de7e8e439eb9418bc4bfc01aa1
MD RIFAQAT ISLAM <rifaqat.islam.11@gmail.com>
2026-09-17 22:08:13 +0600

    Add wsgi.py entry point and update Gunicorn start command for Render deploy

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+﻿import sys
+import os
+
+# Explicitly add current directory to Python path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+from app import app
+
+if __name__ == '__main__':
+    app.run()
